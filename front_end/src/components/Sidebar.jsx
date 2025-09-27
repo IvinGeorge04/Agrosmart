29f7537f8c3147a16bf412b3ac687f56b7876d1d
Arjun Santhosh E N <arjunsanthoshenxthb@gmail.com>
2025-09-27 12:13:42 +0530

    modified sidebar

--- front_end/src/components/Sidebar.jsx
+++ front_end/src/components/Sidebar.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+// Import the icons from react-icons
+import { 
+    FiGrid, 
+    FiCamera, 
+    FiRss, 
+    FiWind, 
+    FiDroplet, 
+    FiBookOpen, 
+    FiAlertTriangle 
+} from 'react-icons/fi';
+import './Sidebar.css'; // We'll create this file for styling
 
 const Sidebar = () => {
     return (
@@ -9,15 +20,28 @@ const Sidebar = () => {
                 <h1>AgroSmart</h1>
             </div>
             <nav className="sidebar-nav">
-                <ul>
-                    <li><NavLink to="/app" end>Dashboard</NavLink></li>
-                    <li><NavLink to="/app/disease-detection">Disease Detection</NavLink></li>
-                    <li><NavLink to="/app/news">Agricultural News</NavLink></li>
-                    <li><NavLink to="/app/weather">Weather</NavLink></li>
-                    <li><NavLink to="/app/irrigation">Irrigation Helper</NavLink></li>
-                    <li><NavLink to="/app/resources">Resources</NavLink></li>
-                    <li><NavLink to="/app/locust-watch">Locust Watch</NavLink></li>
-                </ul>
+                {/* Each NavLink now contains an icon and a span for the text */}
+                <NavLink to="/app" end>
+                    <FiGrid /> <span>Dashboard</span>
+                </NavLink>
+                <NavLink to="/app/disease-detection">
+                    <FiCamera /> <span>Disease Detection</span>
+                </NavLink>
+                <NavLink to="/app/news">
+                    <FiRss /> <span>Agricultural News</span>
+                </NavLink>
+                <NavLink to="/app/weather">
+                    <FiWind /> <span>Weather</span>
+                </NavLink>
+                <NavLink to="/app/irrigation">
+                    <FiDroplet /> <span>Irrigation Helper</span>
+                </NavLink>
+                <NavLink to="/app/resources">
+                    <FiBookOpen /> <span>Resources</span>
+                </NavLink>
+                <NavLink to="/app/locust-watch">
+                    <FiAlertTriangle /> <span>Locust Watch</span>
+                </NavLink>
             </nav>
         </aside>
     );
